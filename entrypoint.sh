1e018d19a4762f2a6e9de705a66c8b76abb5fa35
Luna Versity <18021969+Lunaversitay@users.noreply.github.com>
2021-01-18 20:21:01 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-java -version
-
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
